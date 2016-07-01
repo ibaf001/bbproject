b759fe69a1474ff3c157ea3f09b83c044850823d
ibo <ibo.b@tcs.com>
2016-07-01 10:59:13 -0400

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,16 +4,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Loba"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Loba"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About | Loba"
   end
 
 end
