27eff228bb597cd428666c3e9f388e613e6eb5eb
ibo <ibobafumba@gmail.com>
2016-07-02 02:31:56 -0400

    Add validations to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160702025204) do
+ActiveRecord::Schema.define(version: 20160702062509) do
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
@@ -19,5 +19,7 @@ ActiveRecord::Schema.define(version: 20160702025204) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,87 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @user = User.new(username: "User_test", email: "mama@gmail.com")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "username should be present" do
+    @user.username = ""
+    assert_not @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = ""
+    assert_not @user.valid?
+  end
+
+  test "username should not be too long" do
+    @user.username = "x" * 51
+    assert_not @user.valid?
+  end
+
+  test "email should not be too long" do
+    @user.email = "x" * 101
+    assert_not @user.valid?
+  end
+
+
+  test "email should be valid address" do
+    valid_addresses = %w[user@example.com USER@goo.COM A_US-ER@foo.bar.org first.last@foo.jp mongo+ka@foo.cn]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?
+    end
+
+  end
+
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[user@example,com use_at_goo.com user.name@gmail. foo@gat_hsj.com foo@bar+bas.com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?
+    end
+
+  end
+
+  test "email address should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
