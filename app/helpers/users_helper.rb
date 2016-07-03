161d57e640f7a11503d2156f7561ea26963de3eb
ibo <ibobafumba@gmail.com>
2016-07-02 22:51:44 -0400

    Add avatar to profile

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+  
+  def gravatar_for(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email) #bd4dac8197751954425b6ef651a1e75a
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: user.username, class: "gravatar")
+  end
+
 end
