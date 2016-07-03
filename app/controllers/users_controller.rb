41ddd6b92bf51dc7444a154ab8da166a93c95a38
ibo <ibobafumba@gmail.com>
2016-07-03 01:07:42 -0400

    Add errors messages for failed sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,19 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_param)
+    if @user.save
+      # do something
+    else
+      render 'new'
+    end
+  end
+
+  private
+  
+    def user_param
+      params.require(:user).permit(:username, :email, :password, :password_confirmation)
+    end
+
 end
