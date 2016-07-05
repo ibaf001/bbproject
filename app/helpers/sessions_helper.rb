210d265ff5f19b3180de738eff8bcb8532680a0f
ibo <ibo.b@tcs.com>
2016-07-05 13:52:06 -0400

    Complete logout process

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,6 +3,12 @@ module SessionsHelper
 
   def log_in(user)
     session[:user_id] = user.id
+
+
+  #log out helper
+  def log_out
+    session[:user_id] = nil
+    @current_user = nil  end
   end
 
   #return current user logged in
