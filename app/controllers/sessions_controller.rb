781d3fdd7ff6e55434c1c6fa0fa9e16f72644fec
ibo <ibo.b@tcs.com>
2016-07-05 10:38:16 -0400

    Add sessions controller for login log out

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+
+  def create
+    user = User.find_by(email: params[:session][:email])
+    if user && user.authenticate(params[:session][:email])
+    else
+      flash[:danger] = "Invalid email/password combination"
+      render 'new'
+    end
+    
+    #debugger
+  end
+
+  def destroy
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   get 'about' =>'static_pages#about'
 
   get 'signup' =>'users#new'
+ 
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
 
   resources :users
 
