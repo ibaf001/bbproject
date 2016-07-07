ed82fe704167b6aa858e7eebd309972b4ce41743
ibo <ibo.b@tcs.com>
2016-07-07 15:02:36 -0400

    Add controller for password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
  
 
+ # get 'password_resets/new'
+ # get 'password_resets/edit'
  # get 'account_activations/edit'
 
   get 'home' => 'static_pages#home'
@@ -15,6 +17,7 @@ Rails.application.routes.draw do
 
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
