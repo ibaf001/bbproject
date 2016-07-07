acc9b272eb921e88adf273bcc7f76391b8d6aa65
ibo <ibo.b@tcs.com>
2016-07-07 13:49:51 -0400

    Add activating account

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,9 +8,14 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email])
    # debugger
     if user && user.authenticate(params[:session][:password])
-      log_in user
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_back_or user
+      if user.activated?
+        log_in user
+        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+        redirect_back_or user
+      else
+        flash[:warning] = "Account no activated. Please check your email."
+        redirect_to root_url
+      end
     else
       flash.now[:danger] = "Invalid email/password combination"
       render 'new'
