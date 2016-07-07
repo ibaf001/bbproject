df2f323504a80421680245b040751389c44ed283
ibo <ibo.b@tcs.com>
2016-07-07 16:12:36 -0400

    Complete create action for password_resets controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160707133431) do
+ActiveRecord::Schema.define(version: 20160707190348) do
 
   create_table "users", force: :cascade do |t|
     t.string   "username",          limit: 255
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 20160707133431) do
     t.string   "activation_digest", limit: 255
     t.boolean  "activated"
     t.datetime "activated_at"
+    t.string   "reset_digest",      limit: 255
+    t.datetime "reset_sent_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
