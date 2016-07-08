b3e47142f988e04754e1de0ddfbee170fdad0e0f
ibo <ibo.b@tcs.com>
2016-07-08 15:15:33 -0400

    Show micropost on user profile page + integration test

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,10 @@ User.create!(username: "ibo", email: "ibo@gmail.com", password: "password", pass
   User.create!(username: "user#{n}", email: "user#{n}@example.com" , password: "password", password_confirmation: "password",activated: true, activated_at: Time.now)
 
 end
+
+users = User.order(:created_at).take(6)
+50.times do
+  content = Faker::Lorem.sentence(5)
+  users.each { |user| user.microposts.create!(content: content)}
+
+end
