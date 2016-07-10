58727be0dc37ee1204ac33bb0360352e638617b9
ibo <ibobafumba@gmail.com>
2016-07-09 21:10:56 -0400

    Add micropost delete

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
+  before_action :correct_user, only: [:destroy]
 
 
   def create
@@ -15,6 +16,9 @@ class MicropostsController < ApplicationController
 
 
   def destroy
+    @micropost.destroy
+    flash[:success] = 'Post deleted successfully'
+    redirect_to request.referrer || root_url
   end
 
 
@@ -22,5 +26,10 @@ class MicropostsController < ApplicationController
  
     def micropost_params
       params.require(:micropost).permit(:content)
+    end
+
+    def correct_user
+      @micropost = current_user.microposts.find_by(id: params[:id])
+      redirect_to root_url if @micropost.nil?
     end
 end
