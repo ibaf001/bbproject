c10c5ddf62131258d0689a1bce015eef674a9f3a
ibo <ibo.b@tcs.com>
2016-07-12 16:10:48 -0400

    Add image to the post

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,7 +2,18 @@ class Micropost < ActiveRecord::Base
   belongs_to :user
 
   default_scope -> { order(created_at: :desc) }
-
+  mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :content, presence: true, length: {maximum: 150}
+  validate :picture_size
+
+
+  private 
+  
+    #vaidate the size of uploaded file
+    def picture_size
+      if picture.size > 5.megabyte
+        errors.add(:picture, "Size should be less than 1MB")
+      end
+    end
 end
